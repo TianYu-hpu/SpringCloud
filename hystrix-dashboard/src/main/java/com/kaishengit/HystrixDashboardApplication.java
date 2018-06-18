350be6f67893ad37aaded95bd66be99cac6ce35e
tianyu <tianyu.hpu@gmail.com>
2018-06-18 13:52:24 +0800

    zuul

--- hystrix-dashboard/src/main/java/com/kaishengit/HystrixDashboardApplication.java
+++ hystrix-dashboard/src/main/java/com/kaishengit/HystrixDashboardApplication.java
@@ -1,4 +1,4 @@
-package com.kaishengit.hystrixdashboard;
+package com.kaishengit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
