b07a773bee3dfda769e6bc5f9879f4eaac7d3202
tianyu <tianyu.hpu@gmail.com>
2021-02-23 18:39:04 +0800

    kafka

---
+++ kafka/src/main/java/cn/com/zenmaster/kafka/KafkaApplication.java
@@ -0,0 +1,15 @@
+package cn.com.zenmaster.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+
+@SpringBootApplication
+@EnableBinding(ShopChannel.class)
+public class KafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaApplication.class, args);
+    }
+
+}
